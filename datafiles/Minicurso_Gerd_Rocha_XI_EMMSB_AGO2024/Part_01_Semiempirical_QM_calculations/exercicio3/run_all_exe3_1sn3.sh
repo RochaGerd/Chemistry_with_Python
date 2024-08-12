58467f323cec3377bda46f183bd8eb93c352dbb7
RochaGerd <69326142+RochaGerd@users.noreply.github.com>
2024-08-12 19:57:52 -0300

    Add files via upload

---
+++ datafiles/Minicurso_Gerd_Rocha_XI_EMMSB_AGO2024/Part_01_Semiempirical_QM_calculations/exercicio3/run_all_exe3_1sn3.sh
@@ -0,0 +1,32 @@
+rm *.mop
+cat ../keywords_exe3.txt 1SN3_01.pdb > 1SN3_01.mop
+sed -i 's/INSERT NAME MOL/1SN3_01/g' 1SN3_01.mop
+mopac 1SN3_01
+cat ../keywords_exe3.txt 1SN3_02.pdb > 1SN3_02.mop
+sed -i 's/INSERT NAME MOL/1SN3_02/g' 1SN3_02.mop
+mopac 1SN3_02
+cat ../keywords_exe3.txt 1SN3_03.pdb > 1SN3_03.mop
+sed -i 's/INSERT NAME MOL/1SN3_03/g' 1SN3_03.mop
+mopac 1SN3_03
+cat ../keywords_exe3.txt 1SN3_04.pdb > 1SN3_04.mop
+sed -i 's/INSERT NAME MOL/1SN3_04/g' 1SN3_04.mop
+mopac 1SN3_04
+cat ../keywords_exe3.txt 1SN3_05.pdb > 1SN3_05.mop
+sed -i 's/INSERT NAME MOL/1SN3_05/g' 1SN3_05.mop
+mopac 1SN3_05
+cat ../keywords_exe3.txt 1SN3_06.pdb > 1SN3_06.mop
+sed -i 's/INSERT NAME MOL/1SN3_06/g' 1SN3_06.mop
+mopac 1SN3_06
+cat ../keywords_exe3.txt 1SN3_07.pdb > 1SN3_07.mop
+sed -i 's/INSERT NAME MOL/1SN3_07/g' 1SN3_07.mop
+mopac 1SN3_07
+cat ../keywords_exe3.txt 1SN3_08.pdb > 1SN3_08.mop
+sed -i 's/INSERT NAME MOL/1SN3_08/g' 1SN3_08.mop
+mopac 1SN3_08
+cat ../keywords_exe3.txt 1SN3_09.pdb > 1SN3_09.mop
+sed -i 's/INSERT NAME MOL/1SN3_09/g' 1SN3_09.mop
+mopac 1SN3_09
+cat ../keywords_exe3.txt 1SN3_10.pdb > 1SN3_10.mop
+sed -i 's/INSERT NAME MOL/1SN3_10/g' 1SN3_10.mop
+mopac 1SN3_10
+
